9c11218dc35510b1f8aa9167cbd48d6badf87310
arjun98k <arjunkandekar14@gmail.com>
2024-05-01 11:31:11 +0530

    first commit

--- diff_data_types.rb
+++ diff_data_types.rb
@@ -7,3 +7,9 @@ if ak == 98
 else
     puts "he is akash"
 end
+
+# differnt types of variable in ruby languages
+# global variable => $var
+# class variable  => @@var
+# instance variable => @var
+# local variable => var
