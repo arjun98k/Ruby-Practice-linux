8df19054a67659e4d75a780077284a080b6dbf1a
arjun98k <arjunkandekar14@gmail.com>
2024-06-13 23:56:02 +0530

    swap

---
+++ Ak.java
@@ -0,0 +1,9 @@
+public class Ak{
+public static void main(String[] args) {
+    int j = 0;
+    int end = 5;
+    while (j < end) {
+        System.out.println("hello bhava");
+    }
+}
+}
--- loop.rb
+++ loop.rb
@@ -1,6 +1,6 @@
 a = "arjun love ruby 98 times"
 
-for b in 1..10 do
+for b in 1..20 do
     puts a
   
 end
